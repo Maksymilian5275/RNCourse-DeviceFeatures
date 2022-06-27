05e37d127c8c24c07d9349b184790b5230e75fe3
Maksymilian5275 <karlcoogan1998@gmail.com>
2022-06-27 19:44:59 +0100

    First commit

---
+++ components/Places/LocationPicker.js
@@ -0,0 +1,100 @@
+import { Image, StyleSheet, View, Text } from "react-native";
+import { Colours } from "../../constants/colours";
+import OutlinedButton from "../UI/OutlinedButton";
+import {
+    getCurrentPositionAsync,
+    useForegroundPermissions,
+    PermissionStatus,
+} from "expo-location";
+import { useState } from "react";
+import { getMapPreview } from "../../util/location";
+
+function LocationPicker() {
+    const [pickedLocation, setPickedLocation] = useState();
+
+    const [locationPermissionInformation, requestPermission] =
+        useForegroundPermissions();
+    async function verifyPermissions() {
+        if (
+            locationPermissionInformation.status ===
+            PermissionStatus.UNDETERMINED
+        ) {
+            const permissionResponse = await requestPermission();
+
+            return permissionResponse.granted;
+        }
+
+        if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+            alert("Insufficient location permissions.");
+            return false;
+        }
+
+        return true;
+    }
+
+    async function getLocationHandler() {
+        const hasPermission = await verifyPermissions();
+
+        if (!hasPermission) {
+            return;
+        }
+
+        const location = await getCurrentPositionAsync();
+        setPickedLocation({
+            lat: location.coords.latitude,
+            lng: location.coords.longitude,
+        });
+    }
+
+    function pickOnMapHandler() {}
+
+    let locationPreview = <Text>No location picked yet.</Text>;
+
+    if (pickedLocation) {
+        locationPreview = (
+            <Image
+            style={styles.image}
+                source={{
+                    uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+                }}
+            />
+        );
+    }
+
+    return (
+        <View>
+            <View style={styles.mapPreview}>{locationPreview}</View>
+            <View style={styles.actions}>
+                <OutlinedButton icon="location" onPress={getLocationHandler}>
+                    Locate User
+                </OutlinedButton>
+                <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+                    Pick on Map
+                </OutlinedButton>
+            </View>
+        </View>
+    );
+}
+
+export default LocationPicker;
+
+const styles = StyleSheet.create({
+    mapPreview: {
+        width: "100%",
+        height: 200,
+        marginVertical: 8,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: Colours.primary100,
+        borderRadius: 4,
+    },
+    actions: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+    },
+    image:{
+        width:"100%",
+        height:"100%"
+    },
+});
---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colours } from "../../constants/colours";
+import ImagePicker from "./ImagePicker";
+import LocationPicker from "./LocationPicker";
+
+function PlaceForm() {
+    const [enteredTitle, setEnteredTitle] = useState("");
+
+    function changeTitleHandler(enteredText) {
+        setEnteredTitle(enteredText);
+    }
+
+    return (
+        <ScrollView style={styles.form}>
+            <View>
+                <Text style={styles.label}>Title</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={changeTitleHandler}
+                    value={enteredTitle}
+                />
+            </View>
+            <ImagePicker />
+            <LocationPicker />
+        </ScrollView>
+    );
+}
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({
+    form: {
+        flex: 1,
+        padding: 24,
+    },
+    label: {
+        fontWeight: "bold",
+        marginBottom: 4,
+        color: Colours.primary500,
+    },
+    input: {
+        marginVertical: 8,
+        paddingHorizontal: 4,
+        paddingVertical: 8,
+        fontSize: 16,
+        borderBottomColor: Colours.primary700,
+        borderBottomWidth: 2,
+        backgroundColor: Colours.primary100,
+    },
+});
